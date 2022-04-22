bf102a6411b1648a425f16b1ccfa6ef7cd4fc5b8
GoranAviani <goranaviani@gmail.com>
2022-04-22 20:54:40 +0200

    /react_birthday_reminder_freecodecamp.org

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return (
-      <main>
-        <section className='container'>
-          <h3>
-              birthdays today
-          </h3>
-            <List/>
-            <button onClick={()=>console.log("click")}>Clear All</button>
-        </section>
-      </main>
-  )
+    const [people, setPeople] = useState(data)
+
+
+    return (
+        <main>
+            <section className='container'>
+                <h3>
+                    birthdays today
+                </h3>
+                <List people={people}/>
+                <button onClick={() => console.log("click")}>Clear All</button>
+            </section>
+        </main>
+    )
 }
 
 export default App;
